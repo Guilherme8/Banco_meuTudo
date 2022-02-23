ac674c767b7ac6541be612c34102ac5c05912f9a
Guilherme Farias <guilherme.nf.gf@gmail.com>
2022-02-22 22:53:44 -0300

    Update data.sql

--- banco/src/main/resources/data.sql
+++ banco/src/main/resources/data.sql
@@ -3,8 +3,3 @@ INSERT INTO USUARIO(nome, email, senha) VALUES('Debora', 'debora@email.com', '$2
 
 INSERT INTO CONTA(count_number, balance, usuario_id) VALUES(123, 6000.00, 1);
 INSERT INTO CONTA(count_number, balance, usuario_id) VALUES(124, 10000.00, 2);
-
-INSERT INTO TRANSACTION(dt_scheduling, balance, installments) VALUES('2022-05-05', 1000.00, 1);
-
-INSERT INTO TRANSACTION_CONTA(conta_id, TRANSACTION_ID) VALUES(1, (select id from TRANSACTION where dt_scheduling = '2022-05-05'));
-INSERT INTO TRANSACTION_CONTA(conta_id, TRANSACTION_ID) VALUES(2, (select id from TRANSACTION where dt_scheduling = '2022-05-05'));
